75959d89fe5e43aeec231e2a96775a19acd76433
ksr <ksrlogic@naver.com>
2020-08-12 01:04:23 +0900

    Solve Knum with JS

--- Programmers/Knum.js
+++ Programmers/Knum.js
@@ -2,30 +2,19 @@ function solution(array, commands) {
   let answer = [];
   commands.map((command) => {
     // [2,5,3]
-
     const start = command[0] - 1;
     const finish = command[1];
     const wanted = command[2] - 1;
-    if (start == finish) {
+    if (start + 1 === finish) {
       answer.push(array[start]);
     } else {
-      const slideArray = array.slice(start, finish);
-      slideArray.sort();
+      const slidedArray = array.slice(start, finish);
+      slidedArray.sort((a, b) => a - b);
 
-      const result = slideArray[wanted];
+      const result = slidedArray[wanted];
       answer.push(result);
     }
   });
   return answer;
 }
-
-console.log(
-  solution(
-    [1, 5, 2, 6, 3, 7, 4],
-    [
-      [2, 5, 3],
-      [4, 4, 1],
-      [1, 7, 3],
-    ]
-  )
-);
+// JS에서 sort 메소드는 두 자리수와 한 자리수를 sort할 때 오류가 있는 듯 하다.. return a-b를 해준 모습.
